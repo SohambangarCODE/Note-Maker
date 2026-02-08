d2e3e9334e58cb21b130db500f2369f951d0f958
Soham Bangar <sohambangar188@gmail.com>
2026-02-09 00:27:39 +0530

    fix wildcard route crash

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -80,12 +80,17 @@ app.patch("/api/notes/:id", async (req, res)=>{
 
 
 // SPA fallback
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
 
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "../public/index.html"));
 // });
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"))
+})
+
 
 module.exports = app
