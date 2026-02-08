2363c256f84f4b97b22a4fc20302510322f3d15f
Soham Bangar <sohambangar188@gmail.com>
2026-02-08 14:37:55 +0530

    post get delete functionaly added!

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,11 @@
 const express= require('express')
 const noteModel = require('./Models/note.model')
+const cors = require('cors')
+
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 /**
  * - POST /api/notes
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,64 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios"
 
 function App() {
 
   const [ notes, setNotes ] = useState([])
 
-  axios.get('http://localhost:3000/api/notes')
+  function fetchNotes() {
+    axios.get('http://localhost:3000/api/notes')
   .then((res)=>{
-    setNotes(res.data.notes)
+    setNotes(res.data.note)
   })
+  }
+ 
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    const title = e.target.title.value
+    const description = e.target.description.value
+
+    axios.post('http://localhost:3000/api/notes',{
+      title:title,
+      description:description
+    })
+     .then((res)=>{
+      console.log(res.data);
+      fetchNotes()
+     })
+  }
+
+  function handleDeleteNote(id){
+    axios.delete(`http://localhost:3000/api/notes/${id}`)
+    .then((res)=>{
+      fetchNotes()
+    })
+  }
+
 
   return (
     <>
+    <h1>Notes</h1>
+      <form
+       onSubmit={handleSubmit}
+       className='note-create-form'>
+        <input name='title' type="text" placeholder='enter title' />
+        <input name='description' type="text" placeholder='enter description' />
+        <button>Add Note</button>
+      </form>
+
       <div className="notes">
         {
           notes.map(note => {
-           return <div className="note">
+          return <div className="note">
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+              <button onClick={()=>{
+                handleDeleteNote(note._id)
+              }}>delete</button>
             </div>
           })
         }
